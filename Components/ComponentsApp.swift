2dcfddc1d1fc89d88046f4c9a993dc2901c7882d
berivan dağcı <100922483+berivan0108@users.noreply.github.com>
2026-09-10 15:59:44 +0300

    Initial Commit

---
+++ Components/ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsApp.swift
+//  Components
+//
+//  Created by beri on 10.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComponentsTests/ComponentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsTests.swift
+//  ComponentsTests
+//
+//  Created by beri on 10.09.2026.
+//
+
+import Testing
+@testable import Components
+
+struct ComponentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
